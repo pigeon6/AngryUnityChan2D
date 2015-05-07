add6566b0762dc8ffdcc46a0f83cd1fb382bef9e
Hiroki Omae <pegion6@gmail.com>
2015-05-08 08:52:31 +0900

    initial checkin

---
+++ Assets/Plugins/UnityPerf/CrashReport.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.CrashLog;
+
+public class CrashReport : MonoBehaviour {
+
+	void Awake() {
+		CrashReporting.Init("2642ef7f-3ff6-40fc-b135-45ecec703ff3", "1.0.0", "UserNameHoge");
+	}
+}
---
+++ Assets/Plugins/UnityPerf/ThrowMeAnException.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.CrashLog;
+using System.Collections;
+
+public class ThrowMeAnException : MonoBehaviour 
+{
+	public GameObject obj;
+	private Transform t = null;
+
+	public void CrashForMe()
+	{
+		throw new Exception("Button press exception");
+	}
+
+	public void DoUnassignedNow() {
+
+		// obj reference is not set
+		obj.transform.Translate( Vector3.zero );
+	}
+
+	public void DoNullReferenceNow() {
+		
+		// obj reference is not set
+		t.Translate( Vector3.zero );
+	}
+}
